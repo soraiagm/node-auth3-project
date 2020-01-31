3f77dde841faf6fd61d5e75423838e9fd1375794
Soraia <soraia@Soraias-iMac.local>
2020-01-31 10:48:10 -0500

    completed mvp

---
+++ authentication/restricted-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const { jwtSecret } = require('../config/secrets.js')
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if(token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if(err) {
+        // the token is not valid
+        res.status(401).json({ you: "can't touch this!" })
+      } else {
+        req.user = decodedToken.user;
+      
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' })
+  }
+};
